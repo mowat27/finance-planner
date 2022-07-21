a06ae5c65ac17335dab812c3ed46a5182873f70d
Adrian Mowat <adrian.mowat@gmail.com>
2022-07-21 23:24:20 +0100

    feat: show number of transactions in the summary

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,3 +1,14 @@
-const Summary = () => <h2>Summary</h2>;
+import useTransactions from "../state/transactions/use";
+
+const Summary = () => {
+  const { state } = useTransactions();
+
+  return (
+    <div>
+      <h2>Summary</h2>
+      <p>You have {state.transactions.length} transactions</p>
+    </div>
+  );
+};
 
 export default Summary;
